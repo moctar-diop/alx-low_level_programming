b642de17f8b86a0ded43c1cc0776b17466ebac6d
moctar-diop <diopmoctar92i@gmail.com>
2022-09-09 00:13:22 +0000

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print string in console
+ *
+ * Return: 0 to terminate program
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
