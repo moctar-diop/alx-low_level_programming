01c7c29990dfaa761bdda5d652f84d72b73539cb
moctar-diop <diopmoctar92i@gmail.com>
2022-10-05 22:52:48 +0000

    static libraries

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks for a lowercase character
+ * @c: character that is going to be checked
+ * Return: 1 if is lowercase & 0 otherwise
+**/
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
